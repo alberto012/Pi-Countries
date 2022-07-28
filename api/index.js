ca7f9ac0dd9d5e904de686b2cdacf6a77bbfefb5
alberto012 <rechavarria2@gmail.com>
2022-07-28 13:56:24 -0300

    api

--- api/index.js
+++ api/index.js
@@ -23,9 +23,8 @@ const { conn } = require('./src/db.js');
 
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  
-  server.listen(3001, () => {
+conn.sync({ force: true }).then(() => {
+  server.listen(process.env.PORT || 5000, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
